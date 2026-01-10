c0d8d72e8dee7a410d403fcf7381101b0e9aedd2
marek <m.pribela@gmail.com>
2026-01-10 20:57:22 +0100

    project init

---
+++ src/main/java/ai/pribela/glossapp/GlossappApplication.java
@@ -0,0 +1,13 @@
+package ai.pribela.glossapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlossappApplication.class, args);
+	}
+
+}
